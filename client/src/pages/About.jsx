431828d228063701a92bc3c707e3b4a3c1c1662a
SREEKUTTAN N <sreekuttan1248@gmail.com>
2024-05-29 14:10:34 +0530

    home and about pages completed

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      About
+        <div className='px-4 py-12 max-w-2xl mx-auto'>
+     <h1 className='text-3xl font-bold mb-4 text-slate-700'>About Us</h1>
+     <br /><br />
+     <p className='mb-4 text-slate-700'>React Redux is the official react binding for redux. It provides a predictable state container by connecting React components to a centralized store, simplifying data flow and enabling efficient management of application state across components.</p>
+     <br />
+     <p className='mb-4 text-slate-900'>
+     React
+React is a JavaScript library for building user interfaces. It allows you to create reusable UI components that manage their own state. React's key concepts include:
+
+     </p>
+    </div>
     </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,8 +3,16 @@ import React from 'react'
 const Home = () => {
     console.log("hii");
   return (
-    <div>
-      Home
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+     <h1 className='text-3xl font-bold mb-4 text-slate-700'>Welcome To My App</h1>
+     <br /><br />
+     <p className='mb-4 text-slate-700'>React Redux is the official react binding for redux. It provides a predictable state container by connecting React components to a centralized store, simplifying data flow and enabling efficient management of application state across components.</p>
+     <br />
+     <p className='mb-4 text-slate-900'>
+     React
+React is a JavaScript library for building user interfaces. It allows you to create reusable UI components that manage their own state. React's key concepts include:
+
+     </p>
     </div>
   )
 }
